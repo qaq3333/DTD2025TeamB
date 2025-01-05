a39bd2481609f8a266ba3848b233ff1f10a9f9f1
Chika Chang <13810485+chikacc@users.noreply.github.com>
2025-01-05 12:24:34 +0800

    feat: add background

--- Assets/GamepadUISelectionHelper.cs
+++ Assets/GamepadUISelectionHelper.cs
@@ -7,7 +7,9 @@ public class GamepadUISelectionHelper : MonoBehaviour {
 
     void Update() {
         if (Gamepad.current == null || !Gamepad.current.wasUpdatedThisFrame) return;
-        if (EventSystem.current.currentSelectedGameObject != null) return;
+        if (EventSystem.current.currentSelectedGameObject != null
+            && EventSystem.current.currentSelectedGameObject.activeSelf
+            && EventSystem.current.currentSelectedGameObject.activeInHierarchy) return;
         EventSystem.current.SetSelectedGameObject(defaultSelectedGameObject);
     }
 }
